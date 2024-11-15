d63b33a46a7f4c3ee7c8d4d91562c7b614380f1e
Yuri <Jolycougar@gmail.com>
2024-11-15 10:10:28 +0300

    add: product list and detail view

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,17 @@
 import logging
+from .models import Product, Order
+from django.views.generic import ListView, DetailView
 
 log = logging.getLogger(__name__)
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "shop/product_list.html"
+    queryset = Product.objects.filter(archived=False)
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "shop/product_detail.html"
+
