e1d99a81a4ef87aee24e50b1756b1ccfef241250
Yuri <Jolycougar@gmail.com>
2024-11-15 10:41:06 +0300

    add: product cart

--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, AddToCartView, CartView
 
 
 app_name = 'shop'
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="product-list"),
-    path("<int:pk>", ProductDetailView.as_view(), name="product-detail")
+    path("<int:pk>", ProductDetailView.as_view(), name="product-detail"),
+    path('add-to-cart/<int:product_id>/', AddToCartView.as_view(), name='add_to_cart'),
+    path('cart/', CartView.as_view(), name='cart_view'),
 
 ]
 
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 import logging
-from .models import Product, Order
-from django.views.generic import ListView, DetailView
+from .models import Product, Order, Cart, CartItem
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, View
 
 log = logging.getLogger(__name__)
 
@@ -15,3 +16,25 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = "shop/product_detail.html"
 
+
+class AddToCartView(View):
+    def post(self, request, product_id):
+        product = get_object_or_404(Product, id=product_id)
+        cart, created = Cart.objects.get_or_create(user=request.user)
+
+        cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+
+        if not created:
+            cart_item.quantity += 1
+            cart_item.save()
+
+        return redirect('shop:cart_view')
+
+
+class CartView(ListView):
+    model = Cart
+    template_name = "shop/cart.html"
+    context_object_name = "cart"
+
+    def get_queryset(self):
+        return get_object_or_404(Cart, user=self.request.user)
