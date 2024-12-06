dff9fbebbdf864e698dec8b0bd43ad5c4d62a860
Yuri <Jolycougar@gmail.com>
2024-12-06 10:16:19 +0300

    add: review test view in product detail

--- review/models.py
+++ review/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from account.models import CustomUser
+from shop.models import Product
 
-# Create your models here.
+
+class RatingStar(models.Model):
+    value = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.value}'
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.star} - {self.product.name}"
+
+    class Meta:
+        unique_together = ('user', 'product')
+
+
+class Review(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.ForeignKey(RatingStar, on_delete=models.CASCADE, null=True, blank=True)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'Отзыв {self.author.username} для {self.product.name} - Рейтинг: {self.rating}'
