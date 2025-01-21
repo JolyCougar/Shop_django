f18854fc9eda7421266e09c5bc1bb6d45ade50b1
Yuri <Jolycougar@gmail.com>
2025-01-21 14:59:06 +0300

    Install django_redis +add settings for caches

--- shopSite/settings.py
+++ shopSite/settings.py
@@ -160,3 +160,13 @@ CELERY_TIMEZONE = 'UTC'
 
 Configuration.account_id = config("SHOP_ID_SECRET_KEY")
 Configuration.secret_key = config("SHOP_MONEY_SECRET_KEY")
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
