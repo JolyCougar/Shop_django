650619926d7b2233eef6052fa12b117bd1c5b247
Yuri <Jolycougar@gmail.com>
2025-01-24 09:58:52 +0300

    create signal for delete old image for marketing if change image

--- shop/signals.py
+++ shop/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import pre_save, pre_delete, post_save, post_delete
+from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
-from .models import Product, ProductImage
+from .models import Product, ProductImage, Marketing
 import os
 
 
@@ -12,11 +12,13 @@ def delete_old_preview_on_update(sender, instance, **kwargs):
             if os.path.isfile(old_instance.preview.path):
                 os.remove(old_instance.preview.path)
 
+
 @receiver(post_delete, sender=ProductImage)
 def delete_image_on_delete(sender, instance, **kwargs):
     if instance.image:
         if os.path.isfile(instance.image.path):
             os.remove(instance.image.path)
+
 
 @receiver(pre_save, sender=ProductImage)
 def delete_old_image_on_update(sender, instance, **kwargs):
@@ -25,3 +27,16 @@ def delete_old_image_on_update(sender, instance, **kwargs):
         if old_instance.image and old_instance.image != instance.image:
             if os.path.isfile(old_instance.image.path):
                 os.remove(old_instance.image.path)
+
+
+@receiver(pre_save, sender=Marketing)
+def delete_old_avatar(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            old_preview = Marketing.objects.get(pk=instance.pk).image
+        except Marketing.DoesNotExist:
+            old_preview = None
+
+        if old_preview and old_preview != instance.image:
+            if os.path.isfile(old_preview.path):
+                os.remove(old_preview.path)
