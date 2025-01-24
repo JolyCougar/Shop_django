b565715b11590ebb5e2ea1ef5324ed53fc39bd4f
Yuri <Jolycougar@gmail.com>
2025-01-24 08:52:47 +0300

    fix incorrect name instance

--- shop/signals.py
+++ shop/signals.py
@@ -7,7 +7,7 @@ import os
 @receiver(pre_save, sender=Product)
 def delete_old_image_on_update(sender, instance, **kwargs):
     if instance.pk:
-        old_image = Product.objects.get(pk=instance.pk).image
+        old_image = Product.objects.get(pk=instance.pk).images
         if old_image and old_image != instance.image:
             if os.path.isfile(old_image.path):
                 os.remove(old_image.path)
@@ -15,6 +15,6 @@ def delete_old_image_on_update(sender, instance, **kwargs):
 
 @receiver(pre_delete, sender=Product)
 def delete_image_on_delete(sender, instance, **kwargs):
-    if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path)
+    if instance.images:
+        if os.path.isfile(instance.images.path):
+            os.remove(instance.images.path)
